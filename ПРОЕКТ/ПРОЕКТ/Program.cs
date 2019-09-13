7c52350967c9a3edb63fe48c6cb6f99e523b897a
Nastya <duo1288@gmail.com>
2019-09-13 12:03:49 +0300

    Addd,,,

--- ПРОЕКТ/ПРОЕКТ/Program.cs
+++ ПРОЕКТ/ПРОЕКТ/Program.cs
@@ -8,7 +8,7 @@ namespace ПРОЕКТ
         {
             Console.WriteLine("Enter your name:");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Hello,, " + name);
             Console.ReadKey();
         }
     }
