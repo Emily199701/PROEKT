0b8f51a0a5a3f406ae936f789470cbb7278a0853
Nastya <duo1288@gmail.com>
2019-09-13 11:56:25 +0300

    Say Hi

--- ПРОЕКТ/ПРОЕКТ/Program.cs
+++ ПРОЕКТ/ПРОЕКТ/Program.cs
@@ -6,7 +6,9 @@ namespace ПРОЕКТ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
             Console.ReadKey();
         }
     }
